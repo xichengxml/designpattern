95439bf7e223e3559a00ae4f6b7e1a4195904b94
xichengxml <xichengxml@163.com>
2021-02-06 05:43:55 +0800

    门面与调停者

---
+++ src/main/java/com/xicheng/designpattern/p05_facade/T01_Facade.java
@@ -0,0 +1,16 @@
+package com.xicheng.designpattern.p05_facade;
+
+/**
+ * description 门面模式：内部封装对外统一对象
+ * 调停者模式：内部封装统一对象，连接各个模块，消息队列就是典型的调停者模式
+ * TF解决新添加物体的问题
+ * TF-facade
+ * Frame-展示
+ * GameObject-内部逻辑计算
+ * @author xichengxml
+ * @date 2021/2/6 上午 05:30
+ */
+public class T01_Facade {
+
+
+}
